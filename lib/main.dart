3198916177d3179d8f57cb22995aca672ff15645
manuel belmonte <manuelbelmonteoliva@gmail.com>
2024-10-25 10:21:01 +0200

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
