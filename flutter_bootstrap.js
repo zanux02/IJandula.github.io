d01d33da461f7555fe7b5c0c613afaabc2c87e51
zanux02 <zanux02@users.noreply.github.com>
2024-10-25 11:29:13 +0000

    deploy: 97af7b0466fb3ac11f3be4d83ef184c7d057ca9e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"af0f0d559c8a87d912a20971bbd84afc80a54b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2637122759"
+    serviceWorkerVersion: "1692955516"
   }
 });
