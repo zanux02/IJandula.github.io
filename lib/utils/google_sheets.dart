97af7b0466fb3ac11f3be4d83ef184c7d057ca9e
manuel belmonte <manuelbelmonteoliva@gmail.com>
2024-10-25 13:25:37 +0200

    prueba nombre

--- lib/utils/google_sheets.dart
+++ lib/utils/google_sheets.dart
@@ -16,5 +16,6 @@ class GoogleSheets
     static const String dace = "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=1eQ_GPKIBc-ikvKQ_0FD3q-J8okY-g4uVOmyn4g4SFnU&sheet=DACE";
 
     static const String alumnosServicio = 'https://script.google.com/macros/u/1/s/AKfycbyZ_S8DAf-qJbL8-WS3xL3-lUBHCL0gtLhWJ2bcN5PlovwqLcrzY4hzxECqeFll9UT01g/exec?spreadsheetId=1u79XugcalPc4aPcymy9OsWu1qdg8aKCBvaPWQOH187I&sheet=Servicio';
+
 }
 
